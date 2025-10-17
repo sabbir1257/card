04f82b535981fd7eb5e3c68a9263887aa824a6b1
sabbir1257 <masabbir1257@gmail.com>
2025-10-17 21:07:54 +0600

    Add BusinessCardTemplate3 component with enhanced layout and contact features

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,6 @@
 import BusinessCardTemplate from './component/BusinessCardTemplate';
 import BusinessCardTemplate2 from './component/BusinessCardTemplate2';
-
-
-const userData = {
-  name: "David Harrison",
-  title: "Creative Director",
-  intro: "Signature Intro",
-  about:
-    "I'm David Harrison, a Creative Director driven by the power of ideas and the art of visual storytelling. With years of experience crafting visual experiences that inspire and connect.",
-  profileImage: "/image.jpg",
-  logoImage: "/log.jpg",
-  backgroundImage: "/bg-image.png",
-  contact: {
-    phone: "012 5487 663",
-    email: "info@yoursite.com",
-    website: "www.yoursiteurl.com",
-    address: "123 Road State, Country",
-    home: "567 Road State, Country",
-  },
-  social: {
-    facebook: "facebook.com/david.harrison",
-    instagram: "instagram.com/david.harrison",
-    linkedin: "linkedin.com/in/davidharrison",
-  },
-};
+import BusinessCardTemplate3 from './component/BusinessCardTemplate3';
 
 function App() {
   return (
@@ -31,7 +8,8 @@ function App() {
       {/* New Template Component */}
       <div>
         {/* <BusinessCardTemplate data={userData} /> */}
-        <BusinessCardTemplate2 />
+        {/* <BusinessCardTemplate2 /> */}
+        <BusinessCardTemplate3/>
       </div>
     </div>
   )
